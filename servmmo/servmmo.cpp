10e9efdeb2d5c7b4a6ef504b29b851f32104e68d
lochie <disrute@gmail.com>
2025-10-28 11:03:56 +1100

    basic tcp socket

--- servmmo/servmmo.cpp
+++ servmmo/servmmo.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 
+
+#ifdef _WIN32
+#define _WIN32_WINNT 0x0A00
+#endif
+
+#define ASIO_STANDALONE
+#include <asio.hpp>
+#include <asio/ts/buffer.hpp>
+#include <asio/ts/internet.hpp>
+
 int main()
 {
-    std::cout << "Hello World!\n";
+
+    asio::error_code ec;
+    asio::io_context context;
+    asio::ip::tcp::endpoint endpoint(asio::ip::make_address("192.250.237.69", ec), 443);
+
+    asio::ip::tcp::socket socket(context);
+
+    socket.connect(endpoint, ec);
+
+    if (!ec)
+    {
+        std::cout << "Successfully connected." << std::endl;
+    }
+    else {
+        std::cout << "Error establishing connection.\n" << ec.message() << std::endl;
+    }
+
+    return 0;
 }
