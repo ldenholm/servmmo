be7437d6a666d92c1636976c197829243934e542
lochie <disrute@gmail.com>
2025-10-28 13:53:10 +1100

    async waittoken

--- servmmo/servmmo.cpp
+++ servmmo/servmmo.cpp
@@ -1,43 +1,28 @@
-#include <iostream>
-
-
 #ifdef _WIN32
 #define _WIN32_WINNT 0x0A00
 #endif
 
 #define ASIO_STANDALONE
+#include <iostream>
 #include <asio.hpp>
-#include <asio/ts/buffer.hpp>
-#include <asio/ts/internet.hpp>
+
+void print(const std::error_code& e)
+{
+    std::cout << "Guten Tag, Welt!" << std::endl;
+}
 
 int main()
 {
 
     asio::error_code ec;
     asio::io_context context;
-    asio::ip::tcp::endpoint endpoint(asio::ip::make_address("192.250.237.69", ec), 443);
+    asio::steady_timer timer(context, asio::chrono::seconds(5));
+    
+    // Blocking wait.
+    //timer.wait();
+    //std::cout << "Guten Tag, Welt!" << std::endl;
 
-    asio::ip::tcp::socket socket(context);
-
-    socket.connect(endpoint, ec);
-
-    if (!ec)
-    {
-        std::cout << "Successfully connected." << std::endl;
-    }
-    else {
-        std::cout << "Error establishing connection.\n" << ec.message() << std::endl;
-    }
-
-    if (socket.is_open())
-    {
-        std::string req =
-            "GET /index.html HTTP/1.1\r\n"
-            "HOST: darkswordminiatures.com\r\n"
-            "Connection: close\r\n\r\n";
-
-        socket.write_some(asio::buffer(req.data(), req.size()), ec);
-    }
-
+    timer.async_wait(&print);
+    context.run();
     return 0;
 }
