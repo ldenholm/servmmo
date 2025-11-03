689fd7b7b31895daf574521697921099ecfef221
lochie <disrute@gmail.com>
2025-11-03 16:05:31 +1100

    Implement msg push pull operators

--- NetCommon/net_message.h
+++ NetCommon/net_message.h
@@ -23,7 +23,6 @@ namespace smmo
 
 			size_t size() const
 			{
-				body.size
 				return sizeof(header_message<T> + body.size());
 			}
 
@@ -32,6 +31,69 @@ namespace smmo
 			{
 				os << "ID: " << int(msg.header.id) << " Size: " << msg.header.size;
 				return os;
+			}
+
+			/* Push any data into our message body.
+				Example: Let int x, y be some variables.
+						 Let msg be our message<T>.
+
+				Usage: msg << x << y (pushing data in).
+					   msg >> x >> y (pulling data out).
+			*/
+			template<typename DataType>
+			friend message<T>& operator << (message<T>& msg, const DataType& data)
+			{
+				// Check data can be pushed into body vector with no fancy shenanigans.
+				static_assert(std::is_standard_layout<DataType>::value, "Too complicated \
+					to push this data into message body vector. ");
+
+				// Save current body size of message as this is where we begin to insert data.
+				size_t i = msg.body.size();
+
+				// Resize the body to allow additional space to store new data.
+				msg.body.resize(i + sizeof(Datatype));
+
+				// Push (copy) the new data into our message body.
+				std::memcpy(msg.body.data() + i, &data, sizeof(DataType);
+
+				// Recompute the message size.
+				msg.header.size = msg.size();
+				return msg;
+			}
+
+			/* Pull any data out of our message body.
+				Example: Let int x, y be some variables.
+						 Let msg be our message<T>.
+
+				Usage: msg >> x >> y (pulling data out).
+			*/
+			template<typename DataType>
+			friend message<T> operator >> (message<T>& msg, DataType& data)
+			{
+				// Check data can easily be extracted.
+				static_assert(std::is_standard_layout < DataType::value, "Data cannot easily \
+					be extraced.");
+
+				size_t bytes = msg.body.size() - sizeof(DataType);
+
+				/*
+				
+				|--------|---------|
+				| msg    |DataType-|
+				|--------|---------|
+				
+				*/
+
+				std::memcpy(&data, msg.body.data() + bytes, sizeof(DataType);
+
+				// Resize the message body.
+				msg.body.resize(bytes);
+
+				// Recompute the entire message size
+				msg.header.size = msg.size();
+
+				return msg;
+
 			}
 		};
 	}
