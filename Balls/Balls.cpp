50dbc125b4614c2f06598d4bb6223b673e60fc60
lochie <disrute@gmail.com>
2025-11-08 14:22:48 +1100

    Simple collision detection

--- Balls/Balls.cpp
+++ Balls/Balls.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <numbers>
+#include <cmath>
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
 
@@ -39,13 +40,41 @@ public:
 
 	bool OnUserUpdate(float fElapsed) override
 	{
+
+		// Check circles overlap
+		auto DoCirclesOverlap = [](float x1, float y1, float r1,
+			float x2, float y2, float r2)
+			{
+				// Apparently sqrt() is costly so we can just the following:
+				// It's simply a^2 + b^2 = c^2 and check c^2 <= sum of radius'
+				return ((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) <= (r1 + r2) * (r1 + r2));
+
+			};
+		
+		// Collision detection (quadratic, replace with uniform grid).
+		for (auto& ball : vecBalls)
+		{
+			for (auto& target : vecBalls)
+			{
+				if (ball.id != target.id)
+				{
+
+				}
+			}
+		}
+
+		// Handle Input
+		if (GetMouse(0).bHeld)
+		{
+			LaunchBall(GetMouseX(), GetMouseY());
+		}
+		
 		Clear(olc::BLACK);
 
 		// Draw balls
-
 		for (auto &i : vecBalls)
 		{
-			DrawCircle(i.px, i.py, i.radius, olc::CYAN);
+			DrawCircle((int)i.px, (int)i.py, (int)i.radius, olc::CYAN);
 		}
 			
 		return true;
@@ -66,6 +95,15 @@ private:
 
 		b.id = vecBalls.size(); // Assuming we never remove balls from the vector.
 		vecBalls.emplace_back(b);
+	}
+
+	void LaunchBall(int32_t mouseX, int32_t mouseY)
+	{
+		// Determine mouse over which ball.
+		// Do we check cardinality for mouse x/y coords 
+		// within set of points describing the circle? Maybe more simply just find
+		// which pixel is entirely filled by the circle.
+		
 	}
 };
 
