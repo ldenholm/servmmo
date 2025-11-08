3314cb6e83c6d221cbfe4af7618d4754bff0639e
lochie <disrute@gmail.com>
2025-11-08 11:10:28 +1100

    Add circle physics class

--- Balls/Balls.cpp
+++ Balls/Balls.cpp
@@ -2,73 +2,38 @@
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
 
-//using namespace std;
+using namespace std;
 
-class Example : public olc::PixelGameEngine
+
+class CirclePhysics : public olc::PixelGameEngine
 {
 public:
-	Example()
+	CirclePhysics()
 	{
-		sAppName = "Example";
+		// wchar_t, 16bytes instead of 8, native string encoding
+		// for win32 APIs.
+		sAppName = "Circle Physics";
+
 	}
 
 public:
 	bool OnUserCreate() override
 	{
-		// Called once at the start, so create things here
 		return true;
 	}
 
-	bool OnUserUpdate(float fElapsedTime) override
+	bool OnUserUpdate(float fElapsed) override
 	{
-		// called once per frame
-		for (int x = 0; x < ScreenWidth(); x++)
-			for (int y = 0; y < ScreenHeight(); y++)
-				Draw(x, y, olc::Pixel(rand() % 255, rand() % 255, rand() % 255));
 		return true;
 	}
 };
 
-
 int main()
 {
-	Example demo;
-	if (demo.Construct(256, 240, 4, 4))
-		demo.Start();
-
+	CirclePhysics game;
+	if (game.Construct(160, 120, 8, 8))
+		game.Start();
+	else
+		cout << "Could not initialize game";
 	return 0;
 }
-
-
-//class CirclePhysics : public olc::PixelGameEngine
-//{
-//public:
-//	CirclePhysics()
-//	{
-//		// wchar_t, 16bytes instead of 8, native string encoding
-//		// for win32 APIs.
-//		sAppName = "Circle Physics";
-//
-//	}
-//
-//public:
-//	bool OnUserCreate() override
-//	{
-//		return true;
-//	}
-//
-//	bool OnUserUpdate(float fElapsed) override
-//	{
-//		return true;
-//	}
-//};
-//
-//int main()
-//{
-//	CirclePhysics game;
-//	if (game.Construct(160, 120, 8, 8))
-//		game.Start();
-//	else
-//		cout << "Could not initialize game";
-//	return 0;
-//}
